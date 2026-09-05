21f1b4088b2f89dbe93f0c2fa74b28fc75332e00
theSiddhantPatel <spatel44427777@gmail.com>
2026-09-05 09:36:39 +0530

    leetcode potd solve

---
+++ dsaWithMIK_potd/array/stableIndex3904.java
@@ -0,0 +1,20 @@
+public class stableIndex3904 {
+
+  public int firstStableIndex(int[] nums, int k) {
+    int n = nums.length;
+    int[] suffixMin = new int[n];
+    suffixMin[n - 1] = nums[n - 1];
+
+    for (int i = n - 2; i >= 0; i--) {
+      suffixMin[i] = Math.min(nums[i], suffixMin[i + 1]); //calculate right to left
+    }
+    int max = nums[0];
+    for (int i = 0; i < n; i++) {
+      max = Math.max(max, nums[i]);
+      int valid = max - suffixMin[i];
+      if (valid <= k) return i;
+    }
+
+    return -1;
+  }
+}
